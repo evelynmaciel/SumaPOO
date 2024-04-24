f6a076a71375d80dc183ad365c98a90a65740dc1
Your Name <evelynmaciel>
2024-04-23 23:04:17 -0600

    suma

---
+++ MainSuma.java
@@ -0,0 +1,10 @@
+public class MainSuma {
+    public static void main(String[] args) {
+        
+        Suma n1 = new Suma();
+        n1.a = 3;
+        n1.b = 7;
+        System.out.println(n1.sumar());
+
+    }
+}
---
+++ Suma.java
@@ -0,0 +1,9 @@
+public class Suma {
+ 
+    int a,b,r;
+    public int sumar(){
+        r=this.a + this.b;
+        return r;
+    }
+}
+
